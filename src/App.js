896d973deced4d72703805533124fc7c80ea05f5
sathvik <sathvikreddyfootballer@gmail.com>
2025-09-13 17:59:05 +0530

    update frontend with latest changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 /* import AskForm from "./AskForm"; */
-import AskForm2 from "./askform1";
 import AskForm from "./AskForm3";
 
 
@@ -9,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<AskForm />} />
-        <Route path="/2" element={<AskForm2 />} />
       </Routes>
     </Router>
   );
